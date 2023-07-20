d6db59625fc48157212eb3ef478b3e0ceac268d4
dmarsh9000 <136755794+dmarsh9000@users.noreply.github.com>
2023-07-20 21:06:34 +0700

    Update system notifications

---
+++ flower_zebra_island.py
@@ -0,0 +1,21 @@
+# The Learning Lounge
+
+# This Python file will describe how to write 2000 lines of code in a single file.
+
+# Let's start by defining a function. It won't do anything, but it will count as one of the lines.
+
+def function():
+    pass
+
+# We'll also want to define some variables that we can use in our code.
+
+variable1 = "Hello World!"
+variable2 = 123
+variable3 = [1,2,3]
+
+# Now let's write some lines of code. We'll use a for loop to do this.
+
+for i in range(2000):
+    print(variable1, variable2, variable3)
+
+# That's it! We've now written 2000 lines of code in one Python file.
